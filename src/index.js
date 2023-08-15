c07757d15f4db1976c18d6a135117c182dc830f6
andpli <pandre@tut.by>
2023-08-15 20:44:03 +0300

    init: morse task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,21 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+
+    let c = '';
+    for(let i=0; i < expr.length/10; i++){
+   // Log to console
+   let b = expr.substring(i*10,i*10+10);
+   b = b.replace('**********',' ');
+   b = b.replaceAll('10','.');
+   b = b.replaceAll('11','-');
+   b = b.replaceAll('0','');
+   c = c + (b == ' ' ? b : MORSE_TABLE[b]);
+   
+   
+   
+   }
+   return c;
 }
 
 module.exports = {
